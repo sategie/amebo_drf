d06d95524f10d8a6e0ea6f42e9a8bd9bf74b3ac4
sategie <>
2023-12-21 09:01:17 +0000

    Add post_save signal to Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, blank=True)
@@ -13,3 +14,9 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'{self.user.username} Profile' 
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+post_save.connect(create_profile, sender=User)
