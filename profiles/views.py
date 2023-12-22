da72285c7f4c415d8cc4f006ff1278a201bd2511
sategie <>
2023-12-22 19:05:26 +0000

    Add put method for ProfileDetail view

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from django.http import Http404
+from rest_framework import status
 
 
 class ProfileList(APIView):
@@ -12,5 +14,48 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many= True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    """
+    View which handles the API requests for a single profile object
+    """
+    serializer_class = ProfileSerializer
+
+    def get_object(self, pk):
+        """
+        Retrieves a profile object from the database.
+        Raises an error if the particular profile does not exist
+        """
+        try:
+            profile=Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        """
+        Handles a GET request to the API for a particular profile.
+        Retrieves the profile instance and serializes this into JSON.
+        Returns a response with the serialized profile data
+        """
+        profile = self.get_object(pk)
+        serializer=ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put (self, request, pk):
+        """
+        Handles a PUT request to the API for a particular profile.
+        
+
+        """
+        profile =self.get_object(pk)
+        serializer=ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
