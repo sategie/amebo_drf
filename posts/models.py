8ca136b784f4d0d012732c96155b32c00a7545c9
sategie <>
2023-12-23 16:08:03 +0000

    Create post model and perform migrations

---
+++ posts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    post_content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+    
+    def __str__(self):
+        return self.title
