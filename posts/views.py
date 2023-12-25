3e4c434bf35d981d7ac4a2ab02cd3ec35d097eaa
sategie <>
2023-12-25 13:40:00 +0000

    Add post list GET endpoint

--- posts/views.py
+++ posts/views.py
@@ -4,3 +4,11 @@ from rest_framework.views import APIView
 from .models import Post
 from .serializers import PostSerializer
 
+class PostList(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many = True, context = {'request': request}
+        )
+        return Response(serializer.data)
+
